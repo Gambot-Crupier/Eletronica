0cdbf71b8751086468e8ed2da265a17623e4ccd7
thassio23 <thassio.96@gmail.com>
2019-11-03 22:31:26 +0000

    Update ConectaNarede.py

--- ConectaNarede.py
+++ ConectaNarede.py
@@ -1,20 +1,7 @@
 import RPi.GPIO as GPIO
 import os
 import threading
-
-
-
 import time
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(15, GPIO.OUT)
-GPIO.setup(18, GPIO.OUT)
-
-
-
-NumerodePassos = 800
-
 
 def conectaNaRede():
     os.popen("sudo rfcomm watch hci0 1 getty rfcomm0 115200 vt100 -a pi")
@@ -29,25 +16,6 @@ def checaconexao():
     else:
         return False
 
-def pulso():
-    GPIO.output(15,True)
-    time.sleep(0.001)
-    GPIO.output(15,False)
-    time.sleep(0.001)
-
-def GiraMotor():
-    for i in range(2):
-        GPIO.output(18,True)
-        for i in range(NumerodePassos):
-            pulso()
-        time.sleep(2)
-        GPIO.output(18,False)
-        for i in range(NumerodePassos):
-            pulso()        
-        time.sleep(2)
-
-
-
 ConectaThread=threading.Thread(target=conectaNaRede)
 ConectaThread.daemon = True
 ConectaThread.start()
@@ -55,7 +23,5 @@ ConectaThread.start()
 
 while checaconexao():
     print(".")
-GiraMotor()
-
 
 GPIO.cleanup()
